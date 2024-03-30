2f4b2acd8e75561f88721c242db590554a30240a
Alok <alok.r.kumar@ril.com>
2024-03-30 15:21:24 +0530

    added action on button

--- mySourceControl/ViewController.swift
+++ mySourceControl/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLbl: UILabel!
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
